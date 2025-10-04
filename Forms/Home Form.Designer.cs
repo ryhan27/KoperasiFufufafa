b1e8ad6d73118a1a70a628d0b0f7bc2e14f6ceba
ryhan27 <muhamadrayhanramadhansyah80@gmail.com>
2025-10-04 09:13:05 +0700

    test

---
+++ Forms/Home Form.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace KoperasiFufufafa.Forms
+{
+    partial class Home_Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            menuStrip1 = new MenuStrip();
+            fileToolStripMenuItem = new ToolStripMenuItem();
+            profileToolStripMenuItem = new ToolStripMenuItem();
+            loanToolStripMenuItem = new ToolStripMenuItem();
+            savingToolStripMenuItem = new ToolStripMenuItem();
+            transferToolStripMenuItem = new ToolStripMenuItem();
+            exchangeToolStripMenuItem = new ToolStripMenuItem();
+            historyToolStripMenuItem = new ToolStripMenuItem();
+            panelContent = new Panel();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.ImageScalingSize = new Size(20, 20);
+            menuStrip1.Items.AddRange(new ToolStripItem[] { fileToolStripMenuItem, profileToolStripMenuItem, loanToolStripMenuItem, savingToolStripMenuItem, transferToolStripMenuItem, exchangeToolStripMenuItem, historyToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(899, 28);
+            menuStrip1.TabIndex = 0;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            fileToolStripMenuItem.Size = new Size(46, 24);
+            fileToolStripMenuItem.Text = "File";
+            // 
+            // profileToolStripMenuItem
+            // 
+            profileToolStripMenuItem.Name = "profileToolStripMenuItem";
+            profileToolStripMenuItem.Size = new Size(66, 24);
+            profileToolStripMenuItem.Text = "Profile";
+            // 
+            // loanToolStripMenuItem
+            // 
+            loanToolStripMenuItem.Name = "loanToolStripMenuItem";
+            loanToolStripMenuItem.Size = new Size(55, 24);
+            loanToolStripMenuItem.Text = "Loan";
+            // 
+            // savingToolStripMenuItem
+            // 
+            savingToolStripMenuItem.Name = "savingToolStripMenuItem";
+            savingToolStripMenuItem.Size = new Size(67, 24);
+            savingToolStripMenuItem.Text = "Saving";
+            // 
+            // transferToolStripMenuItem
+            // 
+            transferToolStripMenuItem.Name = "transferToolStripMenuItem";
+            transferToolStripMenuItem.Size = new Size(75, 24);
+            transferToolStripMenuItem.Text = "Transfer";
+            // 
+            // exchangeToolStripMenuItem
+            // 
+            exchangeToolStripMenuItem.Name = "exchangeToolStripMenuItem";
+            exchangeToolStripMenuItem.Size = new Size(86, 24);
+            exchangeToolStripMenuItem.Text = "Exchange";
+            // 
+            // historyToolStripMenuItem
+            // 
+            historyToolStripMenuItem.Name = "historyToolStripMenuItem";
+            historyToolStripMenuItem.Size = new Size(70, 24);
+            historyToolStripMenuItem.Text = "History";
+            // 
+            // panelContent
+            // 
+            panelContent.Dock = DockStyle.Bottom;
+            panelContent.Location = new Point(0, 31);
+            panelContent.Name = "panelContent";
+            panelContent.Size = new Size(899, 479);
+            panelContent.TabIndex = 1;
+            // 
+            // Home_Form
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(899, 510);
+            Controls.Add(panelContent);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "Home_Form";
+            Text = "Home_Form";
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem fileToolStripMenuItem;
+        private ToolStripMenuItem profileToolStripMenuItem;
+        private ToolStripMenuItem loanToolStripMenuItem;
+        private ToolStripMenuItem savingToolStripMenuItem;
+        private ToolStripMenuItem transferToolStripMenuItem;
+        private ToolStripMenuItem exchangeToolStripMenuItem;
+        private ToolStripMenuItem historyToolStripMenuItem;
+        private Panel panelContent;
+    }
+}

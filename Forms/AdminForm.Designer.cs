ecf8659002e9e48afee832323b5207ecba703980
rafal <rafalfinrafin431@gmail.com>
2025-10-04 10:15:27 +0700

    ....

---
+++ Forms/AdminForm.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace KoperasiFufufafa.Forms
+{
+    partial class AdminForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            menuStrip1 = new MenuStrip();
+            fileToolStripMenuItem = new ToolStripMenuItem();
+            memberToolStripMenuItem = new ToolStripMenuItem();
+            accesToolStripMenuItem = new ToolStripMenuItem();
+            productToolStripMenuItem = new ToolStripMenuItem();
+            approvalToolStripMenuItem = new ToolStripMenuItem();
+            accesCooperationToolStripMenuItem = new ToolStripMenuItem();
+            panel1 = new Panel();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { fileToolStripMenuItem, memberToolStripMenuItem, accesToolStripMenuItem, productToolStripMenuItem, approvalToolStripMenuItem, accesCooperationToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(800, 24);
+            menuStrip1.TabIndex = 0;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            fileToolStripMenuItem.Size = new Size(37, 20);
+            fileToolStripMenuItem.Text = "File";
+            // 
+            // memberToolStripMenuItem
+            // 
+            memberToolStripMenuItem.Name = "memberToolStripMenuItem";
+            memberToolStripMenuItem.Size = new Size(64, 20);
+            memberToolStripMenuItem.Text = "Member";
+            // 
+            // accesToolStripMenuItem
+            // 
+            accesToolStripMenuItem.Name = "accesToolStripMenuItem";
+            accesToolStripMenuItem.Size = new Size(55, 20);
+            accesToolStripMenuItem.Text = "Access";
+            // 
+            // productToolStripMenuItem
+            // 
+            productToolStripMenuItem.Name = "productToolStripMenuItem";
+            productToolStripMenuItem.Size = new Size(61, 20);
+            productToolStripMenuItem.Text = "Product";
+            // 
+            // approvalToolStripMenuItem
+            // 
+            approvalToolStripMenuItem.Name = "approvalToolStripMenuItem";
+            approvalToolStripMenuItem.Size = new Size(67, 20);
+            approvalToolStripMenuItem.Text = "Approval";
+            // 
+            // accesCooperationToolStripMenuItem
+            // 
+            accesCooperationToolStripMenuItem.Name = "accesCooperationToolStripMenuItem";
+            accesCooperationToolStripMenuItem.Size = new Size(119, 20);
+            accesCooperationToolStripMenuItem.Text = "Acces Cooperation";
+            // 
+            // panel1
+            // 
+            panel1.Location = new Point(12, 27);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(776, 411);
+            panel1.TabIndex = 1;
+            // 
+            // AdminForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(panel1);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "AdminForm";
+            Text = "AdminForm";
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem fileToolStripMenuItem;
+        private ToolStripMenuItem memberToolStripMenuItem;
+        private ToolStripMenuItem accesToolStripMenuItem;
+        private ToolStripMenuItem productToolStripMenuItem;
+        private ToolStripMenuItem approvalToolStripMenuItem;
+        private ToolStripMenuItem accesCooperationToolStripMenuItem;
+        private Panel panel1;
+    }
+}

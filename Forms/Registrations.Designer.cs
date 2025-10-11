c48c73b36f87bc36c5c06d8224c60a50378331d4
fajar-islamic <fajarislami@DESKTOP-DLIGIGV>
2025-10-11 10:22:50 +0700

    ,,,,

--- Forms/Registrations.Designer.cs
+++ Forms/Registrations.Designer.cs
@@ -1,6 +1,6 @@
-﻿namespace KoperasiFufufafa
+﻿namespace KoperasiFufufafa.Forms
 {
-    partial class registrasi
+    partial class Registrations
     {
         /// <summary>
         /// Required designer variable.
@@ -28,6 +28,8 @@
         /// </summary>
         private void InitializeComponent()
         {
+            button1 = new Button();
+            button2 = new Button();
             label1 = new Label();
             label2 = new Label();
             label3 = new Label();
@@ -50,215 +52,246 @@
             textBox9 = new TextBox();
             textBox10 = new TextBox();
             textBox11 = new TextBox();
-            button1 = new Button();
-            button2 = new Button();
-           
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(75, 409);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 0;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(242, 409);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 29);
+            button2.TabIndex = 1;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(62, 9);
+            label1.Location = new Point(75, 9);
             label1.Name = "label1";
-            label1.Size = new Size(71, 20);
-            label1.TabIndex = 0;
-            label1.Text = "full name";
+            label1.Size = new Size(76, 20);
+            label1.TabIndex = 2;
+            label1.Text = "Full Name";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(341, 9);
+            label2.Location = new Point(342, 9);
             label2.Name = "label2";
             label2.Size = new Size(75, 20);
-            label2.TabIndex = 1;
+            label2.TabIndex = 3;
             label2.Text = "Username";
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(659, 9);
+            label3.Location = new Point(592, 9);
             label3.Name = "label3";
             label3.Size = new Size(70, 20);
-            label3.TabIndex = 2;
+            label3.TabIndex = 4;
             label3.Text = "Password";
             // 
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(62, 91);
+            label4.Location = new Point(75, 75);
             label4.Name = "label4";
             label4.Size = new Size(59, 20);
-            label4.TabIndex = 3;
+            label4.TabIndex = 5;
             label4.Text = "Card ID";
+            label4.Click += label4_Click;
             // 
             // label5
             // 
             label5.AutoSize = true;
-            label5.Location = new Point(341, 91);
+            label5.Location = new Point(342, 75);
             label5.Name = "label5";
             label5.Size = new Size(50, 20);
-            label5.TabIndex = 4;
+            label5.TabIndex = 6;
             label5.Text = "Phone";
+            label5.Click += label5_Click;
             // 
             // label6
             // 
             label6.AutoSize = true;
-            label6.Location = new Point(656, 91);
+            label6.Location = new Point(592, 75);
             label6.Name = "label6";
             label6.Size = new Size(73, 20);
-            label6.TabIndex = 5;
+            label6.TabIndex = 7;
             label6.Text = "Phone Alt";
             // 
             // label7
             // 
             label7.AutoSize = true;
-            label7.Location = new Point(62, 169);
+            label7.Location = new Point(75, 149);
             label7.Name = "label7";
             label7.Size = new Size(46, 20);
-            label7.TabIndex = 6;
+            label7.TabIndex = 8;
             label7.Text = "Email";
             // 
             // label8
             // 
             label8.AutoSize = true;
-            label8.Location = new Point(341, 169);
+            label8.Location = new Point(342, 149);
             label8.Name = "label8";
-            label8.Size = new Size(94, 20);
-            label8.TabIndex = 7;
-            label8.Text = "Reference ID";
+            label8.Size = new Size(50, 20);
+            label8.TabIndex = 9;
+            label8.Text = "label8";
             // 
             // label9
             // 
             label9.AutoSize = true;
-            label9.Location = new Point(659, 169);
+            label9.Location = new Point(592, 149);
             label9.Name = "label9";
-            label9.Size = new Size(53, 20);
-            label9.TabIndex = 8;
-            label9.Text = "Adress";
+            label9.Size = new Size(50, 20);
+            label9.TabIndex = 10;
+            label9.Text = "label9";
             // 
             // label10
             // 
             label10.AutoSize = true;
-            label10.Location = new Point(62, 256);
+            label10.Location = new Point(75, 272);
             label10.Name = "label10";
-            label10.Size = new Size(160, 20);
-            label10.TabIndex = 9;
-            label10.Text = "What is your fav color?";
+            label10.Size = new Size(58, 20);
+            label10.TabIndex = 11;
+            label10.Text = "label10";
             // 
             // label11
             // 
             label11.AutoSize = true;
-            label11.Location = new Point(341, 256);
+            label11.Location = new Point(342, 272);
             label11.Name = "label11";
-            label11.Size = new Size(201, 20);
-            label11.TabIndex = 10;
-            label11.Text = "Who is your fav artist/singer?";
+            label11.Size = new Size(58, 20);
+            label11.TabIndex = 12;
+            label11.Text = "label11";
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(62, 44);
+            textBox1.Location = new Point(75, 188);
             textBox1.Name = "textBox1";
             textBox1.Size = new Size(125, 27);
-            textBox1.TabIndex = 11;
+            textBox1.TabIndex = 13;
             // 
             // textBox2
             // 
-            textBox2.Location = new Point(341, 44);
+            textBox2.Location = new Point(75, 36);
             textBox2.Name = "textBox2";
             textBox2.Size = new Size(125, 27);
-            textBox2.TabIndex = 12;
+            textBox2.TabIndex = 14;
             // 
             // textBox3
             // 
-            textBox3.Location = new Point(659, 44);
+            textBox3.Location = new Point(342, 36);
             textBox3.Name = "textBox3";
             textBox3.Size = new Size(125, 27);
-            textBox3.TabIndex = 13;
+            textBox3.TabIndex = 15;
             // 
             // textBox4
             // 
-            textBox4.Location = new Point(62, 126);
+            textBox4.Location = new Point(75, 104);
             textBox4.Name = "textBox4";
             textBox4.Size = new Size(125, 27);
-            textBox4.TabIndex = 14;
+            textBox4.TabIndex = 16;
             // 
             // textBox5
             // 
-            textBox5.Location = new Point(341, 126);
+            textBox5.Location = new Point(342, 104);
             textBox5.Name = "textBox5";
             textBox5.Size = new Size(125, 27);
-            textBox5.TabIndex = 15;
+            textBox5.TabIndex = 17;
             // 
             // textBox6
             // 
-            textBox6.Location = new Point(656, 126);
+            textBox6.Location = new Point(592, 36);
             textBox6.Name = "textBox6";
             textBox6.Size = new Size(125, 27);
-            textBox6.TabIndex = 16;
+            textBox6.TabIndex = 18;
             // 
             // textBox7
             // 
-            textBox7.Location = new Point(62, 211);
+            textBox7.Location = new Point(592, 104);
             textBox7.Name = "textBox7";
             textBox7.Size = new Size(125, 27);
-            textBox7.TabIndex = 17;
+            textBox7.TabIndex = 19;
             // 
             // textBox8
             // 
-            textBox8.Location = new Point(341, 211);
+            textBox8.Location = new Point(342, 188);
             textBox8.Name = "textBox8";
             textBox8.Size = new Size(125, 27);
-            textBox8.TabIndex = 18;
+            textBox8.TabIndex = 20;
             // 
             // textBox9
             // 
-            textBox9.Location = new Point(588, 211);
+            textBox9.Location = new Point(592, 188);
+            textBox9.Multiline = true;
             textBox9.Name = "textBox9";
-            textBox9.Size = new Size(200, 27);
-            textBox9.TabIndex = 19;
+            textBox9.Size = new Size(178, 104);
+            textBox9.TabIndex = 21;
             // 
             // textBox10
             // 
-            textBox10.Location = new Point(62, 299);
+            textBox10.Location = new Point(75, 324);
             textBox10.Name = "textBox10";
             textBox10.Size = new Size(125, 27);
-            textBox10.TabIndex = 20;
+            textBox10.TabIndex = 22;
             // 
             // textBox11
             // 
-            textBox11.Location = new Point(341, 299);
+            textBox11.Location = new Point(342, 324);
             textBox11.Name = "textBox11";
             textBox11.Size = new Size(125, 27);
-            textBox11.TabIndex = 21;
+            textBox11.TabIndex = 23;
+            textBox11.TextChanged += textBox11_TextChanged;
             // 
-            // button1
+            // Registrations
             // 
-            button1.Location = new Point(62, 358);
-            button1.Name = "button1";
-            button1.Size = new Size(94, 29);
-            button1.TabIndex = 22;
-            button1.Text = "Submit";
-            button1.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            button2.Location = new Point(209, 358);
-            button2.Name = "button2";
-            button2.Size = new Size(94, 29);
-            button2.TabIndex = 23;
-            button2.Text = "Back";
-            button2.UseVisualStyleBackColor = true;
-            // 
-            // registrasi
-            // 
-          
-        
-      
-      
-            
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 547);
+            Controls.Add(textBox11);
+            Controls.Add(textBox10);
+            Controls.Add(textBox9);
+            Controls.Add(textBox8);
+            Controls.Add(textBox7);
+            Controls.Add(textBox6);
+            Controls.Add(textBox5);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label11);
+            Controls.Add(label10);
+            Controls.Add(label9);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "Registrations";
+            Text = "Registrations";
+            Load += Registrations_Load;
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
+        private Button button1;
+        private Button button2;
         private Label label1;
         private Label label2;
         private Label label3;
@@ -281,7 +314,5 @@
         private TextBox textBox9;
         private TextBox textBox10;
         private TextBox textBox11;
-        private Button button1;
-        private Button button2;
     }
 }

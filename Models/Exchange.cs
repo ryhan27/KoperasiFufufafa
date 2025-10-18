bd399e6290ecb8f5fc29a0bc38e4112405e11cda
pandri.pandri <pandri.pandri@wirecard.com>
2025-10-18 09:31:53 +0700

    exchange model

---
+++ Models/Exchange.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KoperasiFufufafa.Models
+{
+    public class Exchange
+    {
+        public int Id { get; set; }
+        [Required] public int MemberId { get; set; }
+        public Member Member { get; set; } = null!;
+        public string ExchangeId { get; set; }
+        public decimal Amount { get; set; }
+        public decimal AmountExchanged {  get; set; }
+        public decimal Fee { get; set; }
+        public decimal Rate { get; set; }
+        public DateTime ExchangeDate { get; set; }
+        public decimal TotalAmountExchanged { get; set; }
+
+    }
+}

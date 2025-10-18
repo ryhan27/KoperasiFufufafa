2849a094efe8df5c199b0b0c1e72827a3325d8a4
Raka Erlangga Ramadhan <Rakaerl@outlook.co.id>
2025-10-18 10:16:45 +0700

    Installment

---
+++ Models/Installment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KoperasiFufufafa.Models
+{
+    internal class Installment
+    {
+        public int Id { get; set; }
+        public int LoanId { get; set; }
+        public Loan Loan { get; set; } = null!;
+        public decimal Amount { get; set; }
+        public DateTime PaymentDate { get; set; } = DateTime.UtcNow;
+        public string? proofPath { get; set; }
+    }
+}

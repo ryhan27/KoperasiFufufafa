5daa3908f4068ccba3745a796064626a4cde56f6
ryhan27 <muhamadrayhanramadhansyah80@gmail.com>
2025-10-18 10:33:52 +0700

    udh woyyy

---
+++ Models/Configuration.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KoperasiFufufafa.Models
+{
+    class Configuration
+    {
+        public int Id { get; set; }
+
+        [MaxLength(4000)]
+        public string terminologi1 { get; set; }
+
+        [MaxLength(4000)]
+        public string terminologi2 { get; set; }
+
+        [MaxLength(4000)]
+        public string terminologi3 { get; set; }
+
+        public decimal exchangeRate { get; set; }
+        public decimal transferAcrossFee { get; set; }
+        public decimal transferInhouseFee { get; set; }
+    }
+}

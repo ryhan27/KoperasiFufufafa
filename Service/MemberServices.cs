567303cb5a52a9f14b794d506b0ab1619c84c546
ryhan27 <muhamadrayhanramadhansyah80@gmail.com>
2025-10-25 09:54:35 +0700

    udh nih member servis

---
+++ Service/MemberServices.cs
@@ -0,0 +1,45 @@
+﻿using KoperasiFufufafa.Data;
+using KoperasiFufufafa.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KoperasiFufufafa.Service
+{
+   public class MemberServices
+    {
+        private readonly AppDbContext _db;
+        public MemberService(AppDbContext db) => _db = db;
+        public List<object> SetDropdown()
+        {
+            var list = _db.Members
+                .OrderBy(m => m.FullName)
+                .Select(m => new
+                {
+                    m.Id,
+                    DisplayName = m.MemberId + " - " + m.FullName
+                })
+                .ToList<object>();
+
+            return list;
+        }
+
+        public Member? FindById(int id)
+        {
+            return _db.Members.FirstOrDefault(x => x.Id == id);
+        }
+
+        public List<Member> SetGrid()
+        {
+            return _db.Members.OrderByDescending(m => m.ModDate).ToList<Member>();
+        }
+
+        public async void Update(Member member)
+        {
+            _db.Members.Update(member);
+            await _db.SaveChangesAsync();
+        }
+    }
+}

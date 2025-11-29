0e9c272eb7fea548f4eb656acf03a7e5bceb4310
snzza <adipratamarico6@gmail.com>
2025-11-29 10:33:22 +0700

    cvc

--- Forms/MemberPage.Designer.cs
+++ Forms/MemberPage.Designer.cs
@@ -39,6 +39,16 @@
             lblStatus = new Label();
             comboBox1 = new ComboBox();
             btnUpdate = new Button();
+            dataGridView1 = new DataGridView();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
             // 
             // lblmember
@@ -56,9 +66,9 @@
             lblCardId.AutoSize = true;
             lblCardId.Location = new Point(36, 73);
             lblCardId.Name = "lblCardId";
-            lblCardId.Size = new Size(42, 15);
+            lblCardId.Size = new Size(73, 15);
             lblCardId.TabIndex = 1;
-            lblCardId.Text = "CardId";
+            lblCardId.Text = "Card Id(KTP)";
             lblCardId.Click += label2_Click;
             // 
             // lblFullName
@@ -142,10 +152,104 @@
             btnUpdate.Text = "Update";
             btnUpdate.UseVisualStyleBackColor = true;
             // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(408, 32);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(511, 373);
+            dataGridView1.TabIndex = 11;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(150, 51);
+            label1.Name = "label1";
+            label1.Size = new Size(17, 15);
+            label1.TabIndex = 12;
+            label1.Text = "__";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(150, 73);
+            label2.Name = "label2";
+            label2.Size = new Size(17, 15);
+            label2.TabIndex = 13;
+            label2.Text = "__";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(150, 96);
+            label3.Name = "label3";
+            label3.Size = new Size(17, 15);
+            label3.TabIndex = 14;
+            label3.Text = "__";
+            label3.Click += label3_Click_1;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(150, 124);
+            label4.Name = "label4";
+            label4.Size = new Size(17, 15);
+            label4.TabIndex = 15;
+            label4.Text = "__";
+            label4.Click += label4_Click;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(150, 149);
+            label5.Name = "label5";
+            label5.Size = new Size(17, 15);
+            label5.TabIndex = 16;
+            label5.Text = "__";
+            label5.Click += label5_Click;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(150, 173);
+            label6.Name = "label6";
+            label6.Size = new Size(17, 15);
+            label6.TabIndex = 17;
+            label6.Text = "__";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(150, 197);
+            label7.Name = "label7";
+            label7.Size = new Size(17, 15);
+            label7.TabIndex = 18;
+            label7.Text = "__";
+            label7.Click += label7_Click;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(150, 221);
+            label8.Name = "label8";
+            label8.Size = new Size(17, 15);
+            label8.TabIndex = 19;
+            label8.Text = "__";
+            label8.Click += label8_Click;
+            // 
             // MemberPage
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(dataGridView1);
             Controls.Add(btnUpdate);
             Controls.Add(comboBox1);
             Controls.Add(lblStatus);
@@ -160,6 +264,7 @@
             Name = "MemberPage";
             Size = new Size(991, 640);
             Load += MemberPage_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -177,5 +282,14 @@
         private Label lblStatus;
         private ComboBox comboBox1;
         private Button btnUpdate;
+        private DataGridView dataGridView1;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
     }
 }
--- Forms/MemberPage.cs
+++ Forms/MemberPage.cs
@@ -46,5 +46,30 @@ namespace KoperasiFufufafa.Forms
         {
 
         }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }

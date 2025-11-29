fa740077fd52d540e6298e9253f13d0df6e23717
rafal <rafalfinrafin431@gmail.com>
2025-11-29 08:52:27 +0700

    masukkk pak eko

---
+++ Api/Models/PayloadResponse.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace KoperasiFufufafa.Api.Models
+{
+    public class CoopApiResponse
+    {
+        public string? CoopCode { get; set; }
+        public string ResponseCode { get; set; } = "";
+        public string ResponseMessage { get; set; } = "";
+        public DateTime ResponseTime { get; set; }
+
+        [JsonPropertyName("coopList")]
+        public List<CoopAcross> CoopList { get; set; } = new();
+    }
+
+    public class MemberApiResponse
+    {
+        public string? MemberCode { get; set; }
+        public string ResponseCode { get; set; } = "";
+        public string ResponseMessage { get; set; } = "";
+        public DateTime ResponseTime { get; set; }
+
+        [JsonPropertyName("memberList")]
+        public List<MemberAcross> MemberList { get; set; } = new();
+    }
+
+    public class BalanceApiResponse
+    {
+        public string ResponseCode { get; set; } = "";
+        public string ResponseMessage { get; set; } = "";
+        public DateTime ResponseTime { get; set; }
+        public string? MemberCode { get; set; }
+        public string? CoopCode { get; set; }
+
+        [JsonPropertyName("balanceList")]
+        public List<BalanceAcross> BalanceList { get; set; } = new();
+    }
+
+    public class TransferApiResponse
+    {
+        public string ResponseCode { get; set; } = "";
+        public string ResponseMessage { get; set; } = "";
+        public DateTime ResponseTime { get; set; }
+        public string? TransactionCode { get; set; }
+
+        [JsonPropertyName("memberList")]
+        public List<TransferAcross> TransferList { get; set; } = new();
+    }
+}

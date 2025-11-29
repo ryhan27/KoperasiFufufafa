847f1d4a37ce39fcc3fc2f2ac68f8f7f1a5c95f4
snzza <adipratamarico6@gmail.com>
2025-11-29 10:26:03 +0700

    fdgvfgdfgfgdg

--- Forms/MemberPage.Designer.cs
+++ Forms/MemberPage.Designer.cs
@@ -28,289 +28,154 @@
         /// </summary>
         private void InitializeComponent()
         {
-            labelMemberId = new Label();
-            labelCardId = new Label();
-            labelFullName = new Label();
-            labelJoinDate = new Label();
-            labelEmail = new Label();
-            labelPhone = new Label();
-            labelPhoneAlt = new Label();
-            labelAddress = new Label();
-            label9 = new Label();
-            comboBox1 = new ComboBox();
-            MemberId = new Label();
+            lblmember = new Label();
             lblCardId = new Label();
             lblFullName = new Label();
             lblJoinDate = new Label();
             lblEmail = new Label();
             lblPhone = new Label();
             lblPhoneAlt = new Label();
-            lblAddres = new Label();
-            dgvMemberx = new DataGridView();
-            button1 = new Button();
-            lblUpdate = new Label();
-            ((System.ComponentModel.ISupportInitialize)dgvMemberx).BeginInit();
+            lbladdress = new Label();
+            lblStatus = new Label();
+            comboBox1 = new ComboBox();
+            btnUpdate = new Button();
             SuspendLayout();
             // 
-            // labelMemberId
+            // lblmember
             // 
-            labelMemberId.AutoSize = true;
-            labelMemberId.Location = new Point(32, 32);
-            labelMemberId.Name = "labelMemberId";
-            labelMemberId.Size = new Size(65, 15);
-            labelMemberId.TabIndex = 0;
-            labelMemberId.Text = "Member Id";
-            labelMemberId.Click += label1_Click;
-            // 
-            // labelCardId
-            // 
-            labelCardId.AutoSize = true;
-            labelCardId.Location = new Point(32, 58);
-            labelCardId.Name = "labelCardId";
-            labelCardId.Size = new Size(76, 15);
-            labelCardId.TabIndex = 1;
-            labelCardId.Text = "Card Id (KTP)";
-            // 
-            // labelFullName
-            // 
-            labelFullName.AutoSize = true;
-            labelFullName.Location = new Point(32, 89);
-            labelFullName.Name = "labelFullName";
-            labelFullName.Size = new Size(61, 15);
-            labelFullName.TabIndex = 2;
-            labelFullName.Text = "Full Name";
-            // 
-            // labelJoinDate
-            // 
-            labelJoinDate.AutoSize = true;
-            labelJoinDate.Location = new Point(32, 115);
-            labelJoinDate.Name = "labelJoinDate";
-            labelJoinDate.Size = new Size(55, 15);
-            labelJoinDate.TabIndex = 3;
-            labelJoinDate.Text = "Join Date";
-            labelJoinDate.Click += label4_Click;
-            // 
-            // labelEmail
-            // 
-            labelEmail.AutoSize = true;
-            labelEmail.Location = new Point(32, 142);
-            labelEmail.Name = "labelEmail";
-            labelEmail.Size = new Size(36, 15);
-            labelEmail.TabIndex = 4;
-            labelEmail.Text = "Email";
-            labelEmail.Click += label5_Click;
-            // 
-            // labelPhone
-            // 
-            labelPhone.AutoSize = true;
-            labelPhone.Location = new Point(32, 169);
-            labelPhone.Name = "labelPhone";
-            labelPhone.Size = new Size(41, 15);
-            labelPhone.TabIndex = 5;
-            labelPhone.Text = "Phone";
-            labelPhone.Click += label6_Click;
-            // 
-            // labelPhoneAlt
-            // 
-            labelPhoneAlt.AutoSize = true;
-            labelPhoneAlt.Location = new Point(32, 193);
-            labelPhoneAlt.Name = "labelPhoneAlt";
-            labelPhoneAlt.Size = new Size(59, 15);
-            labelPhoneAlt.TabIndex = 6;
-            labelPhoneAlt.Text = "Phone Alt";
-            // 
-            // labelAddress
-            // 
-            labelAddress.AutoSize = true;
-            labelAddress.Location = new Point(32, 220);
-            labelAddress.Name = "labelAddress";
-            labelAddress.Size = new Size(44, 15);
-            labelAddress.TabIndex = 7;
-            labelAddress.Text = "Addres";
-            labelAddress.Click += label8_Click;
-            // 
-            // label9
-            // 
-            label9.AutoSize = true;
-            label9.Location = new Point(32, 248);
-            label9.Name = "label9";
-            label9.Size = new Size(39, 15);
-            label9.TabIndex = 8;
-            label9.Text = "Status";
-            label9.Click += label9_Click;
-            // 
-            // comboBox1
-            // 
-            comboBox1.FormattingEnabled = true;
-            comboBox1.Location = new Point(32, 278);
-            comboBox1.Name = "comboBox1";
-            comboBox1.Size = new Size(228, 23);
-            comboBox1.TabIndex = 9;
-            comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
-            // 
-            // MemberId
-            // 
-            MemberId.AutoSize = true;
-            MemberId.Location = new Point(135, 32);
-            MemberId.Name = "MemberId";
-            MemberId.Size = new Size(16, 15);
-            MemberId.TabIndex = 10;
-            MemberId.Text = "...";
-            MemberId.Click += MemberId_Click;
+            lblmember.AutoSize = true;
+            lblmember.Location = new Point(36, 51);
+            lblmember.Name = "lblmember";
+            lblmember.Size = new Size(65, 15);
+            lblmember.TabIndex = 0;
+            lblmember.Text = "Member Id";
+            lblmember.Click += label1_Click;
             // 
             // lblCardId
             // 
             lblCardId.AutoSize = true;
-            lblCardId.Location = new Point(135, 58);
+            lblCardId.Location = new Point(36, 73);
             lblCardId.Name = "lblCardId";
-            lblCardId.Size = new Size(16, 15);
-            lblCardId.TabIndex = 11;
-            lblCardId.Text = "...";
+            lblCardId.Size = new Size(42, 15);
+            lblCardId.TabIndex = 1;
+            lblCardId.Text = "CardId";
+            lblCardId.Click += label2_Click;
             // 
             // lblFullName
             // 
             lblFullName.AutoSize = true;
-            lblFullName.Location = new Point(135, 89);
+            lblFullName.Location = new Point(36, 96);
             lblFullName.Name = "lblFullName";
-            lblFullName.Size = new Size(16, 15);
-            lblFullName.TabIndex = 12;
-            lblFullName.Text = "...";
-            lblFullName.Click += label12_Click;
+            lblFullName.Size = new Size(61, 15);
+            lblFullName.TabIndex = 2;
+            lblFullName.Text = "Full Name";
+            lblFullName.Click += label3_Click;
             // 
             // lblJoinDate
             // 
             lblJoinDate.AutoSize = true;
-            lblJoinDate.Location = new Point(135, 115);
+            lblJoinDate.Location = new Point(36, 124);
             lblJoinDate.Name = "lblJoinDate";
-            lblJoinDate.Size = new Size(16, 15);
-            lblJoinDate.TabIndex = 13;
-            lblJoinDate.Text = "...";
-            lblJoinDate.Click += label13_Click;
+            lblJoinDate.Size = new Size(55, 15);
+            lblJoinDate.TabIndex = 3;
+            lblJoinDate.Text = "Join Date";
             // 
             // lblEmail
             // 
             lblEmail.AutoSize = true;
-            lblEmail.Location = new Point(135, 142);
+            lblEmail.Location = new Point(36, 149);
             lblEmail.Name = "lblEmail";
-            lblEmail.Size = new Size(16, 15);
-            lblEmail.TabIndex = 14;
-            lblEmail.Text = "...";
-            lblEmail.Click += label14_Click;
+            lblEmail.Size = new Size(36, 15);
+            lblEmail.TabIndex = 4;
+            lblEmail.Text = "Email";
             // 
             // lblPhone
             // 
             lblPhone.AutoSize = true;
-            lblPhone.Location = new Point(135, 169);
+            lblPhone.Location = new Point(36, 173);
             lblPhone.Name = "lblPhone";
-            lblPhone.Size = new Size(16, 15);
-            lblPhone.TabIndex = 15;
-            lblPhone.Text = "...";
-            lblPhone.Click += Phone_Click;
+            lblPhone.Size = new Size(41, 15);
+            lblPhone.TabIndex = 5;
+            lblPhone.Text = "Phone";
             // 
             // lblPhoneAlt
             // 
             lblPhoneAlt.AutoSize = true;
-            lblPhoneAlt.Location = new Point(135, 193);
+            lblPhoneAlt.Location = new Point(36, 197);
             lblPhoneAlt.Name = "lblPhoneAlt";
-            lblPhoneAlt.Size = new Size(16, 15);
-            lblPhoneAlt.TabIndex = 16;
-            lblPhoneAlt.Text = "...";
-            lblPhoneAlt.Click += label16_Click;
+            lblPhoneAlt.Size = new Size(59, 15);
+            lblPhoneAlt.TabIndex = 6;
+            lblPhoneAlt.Text = "Phone Alt";
             // 
-            // lblAddres
+            // lbladdress
             // 
-            lblAddres.AutoSize = true;
-            lblAddres.Location = new Point(135, 220);
-            lblAddres.Name = "lblAddres";
-            lblAddres.Size = new Size(16, 15);
-            lblAddres.TabIndex = 17;
-            lblAddres.Text = "...";
-            lblAddres.Click += label17_Click;
+            lbladdress.AutoSize = true;
+            lbladdress.Location = new Point(36, 221);
+            lbladdress.Name = "lbladdress";
+            lbladdress.Size = new Size(49, 15);
+            lbladdress.TabIndex = 7;
+            lbladdress.Text = "Address";
             // 
-            // dgvMemberx
+            // lblStatus
             // 
-            dgvMemberx.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dgvMemberx.Location = new Point(346, 20);
-            dgvMemberx.Name = "dgvMemberx";
-            dgvMemberx.Size = new Size(443, 335);
-            dgvMemberx.TabIndex = 18;
-            dgvMemberx.CellContentClick += dgvMember_CellContentClick;
+            lblStatus.AutoSize = true;
+            lblStatus.Location = new Point(38, 263);
+            lblStatus.Name = "lblStatus";
+            lblStatus.Size = new Size(39, 15);
+            lblStatus.TabIndex = 8;
+            lblStatus.Text = "Status";
             // 
-            // button1
+            // comboBox1
             // 
-            button1.Location = new Point(32, 318);
-            button1.Name = "button1";
-            button1.Size = new Size(75, 23);
-            button1.TabIndex = 19;
-            button1.Text = "Update";
-            button1.UseVisualStyleBackColor = true;
-            button1.Click += button1_Click;
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(38, 281);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(271, 23);
+            comboBox1.TabIndex = 9;
             // 
-            // lblUpdate
+            // btnUpdate
             // 
-            lblUpdate.AutoSize = true;
-            lblUpdate.Location = new Point(135, 322);
-            lblUpdate.Name = "lblUpdate";
-            lblUpdate.Size = new Size(16, 15);
-            lblUpdate.TabIndex = 20;
-            lblUpdate.Text = "...";
-            lblUpdate.Click += label18_Click_1;
+            btnUpdate.Location = new Point(36, 319);
+            btnUpdate.Name = "btnUpdate";
+            btnUpdate.Size = new Size(75, 23);
+            btnUpdate.TabIndex = 10;
+            btnUpdate.Text = "Update";
+            btnUpdate.UseVisualStyleBackColor = true;
             // 
             // MemberPage
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            Controls.Add(lblUpdate);
-            Controls.Add(button1);
-            Controls.Add(dgvMemberx);
-            Controls.Add(lblAddres);
+            Controls.Add(btnUpdate);
+            Controls.Add(comboBox1);
+            Controls.Add(lblStatus);
+            Controls.Add(lbladdress);
             Controls.Add(lblPhoneAlt);
             Controls.Add(lblPhone);
             Controls.Add(lblEmail);
             Controls.Add(lblJoinDate);
             Controls.Add(lblFullName);
             Controls.Add(lblCardId);
-            Controls.Add(MemberId);
-            Controls.Add(comboBox1);
-            Controls.Add(label9);
-            Controls.Add(labelAddress);
-            Controls.Add(labelPhoneAlt);
-            Controls.Add(labelPhone);
-            Controls.Add(labelEmail);
-            Controls.Add(labelJoinDate);
-            Controls.Add(labelFullName);
-            Controls.Add(labelCardId);
-            Controls.Add(labelMemberId);
+            Controls.Add(lblmember);
             Name = "MemberPage";
-            Size = new Size(882, 445);
+            Size = new Size(991, 640);
             Load += MemberPage_Load;
-            ((System.ComponentModel.ISupportInitialize)dgvMemberx).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
 
-        private Label labelMemberId;
-        private Label labelCardId;
-        private Label labelFullName;
-        private Label labelJoinDate;
-        private Label labelEmail;
-        private Label labelPhone;
-        private Label labelPhoneAlt;
-        private Label labelAddress;
-        private Label label9;
-        private ComboBox comboBox1;
-        private Label MemberId;
+        private Label lblmember;
         private Label lblCardId;
         private Label lblFullName;
         private Label lblJoinDate;
         private Label lblEmail;
         private Label lblPhone;
         private Label lblPhoneAlt;
-        private Label lblAddres;
-        private DataGridView dgvMemberx;
-        private Button button1;
-        private Label lblUpdate;
+        private Label lbladdress;
+        private Label lblStatus;
+        private ComboBox comboBox1;
+        private Button btnUpdate;
     }
 }
